caf53f7a0d8c1d819e70598e26948caf79f3af70
Thiago Schizzi <thiagoschizzi@hotmail.com>
2023-06-27 01:32:52 -0300

    Arquivo do usuário

--- User.java
+++ User.java
@@ -1,1 +1,9 @@
-Thiago Schizzi
+import java.util.Scanner;
+
+public class User {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.printf("insira seu nome completo: ");
+        myScanner.nextLine();
+    }
+}
